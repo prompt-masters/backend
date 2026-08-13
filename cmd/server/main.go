a8179fe0d0387d414539aa3c9c1642a6a3190bdc
Khang Le <ngukhangle@gmail.com>
2026-08-13 16:08:14 +0700

    feat/PA15: setting up routers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("hello prompt masters!")
+	// loading env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	host := os.Getenv("HOST")
+	dbURL := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Invalid connection string format: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	}
+
+	mux := http.NewServeMux()
+
+	s := http.Server{
+		Addr:    net.JoinHostPort(host, port),
+		Handler: mux,
+	}
+
+	log.Printf("Running server on %s", s.Addr)
+	log.Fatal(s.ListenAndServe())
 }
