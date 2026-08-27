93a666a5ba8594e80515f31c1cdfb3c25e842f95
Khang Le <ngukhangle@gmail.com>
2026-08-27 14:02:44 +0700

    feat: user and token domain

---
+++ internal/domain/user.go
@@ -0,0 +1,16 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID            uuid.UUID `json:"id"`
+	Email         string    `json:"email"`
+	Password      string    `json:"-"`
+	EmailVerified bool      `json:"email_verified"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
